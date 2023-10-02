3eec334d8200c5d1ad133ff639f3068398f7be41
Ilka <Ilka@LAPTOP-VAVOB6QC>
2023-10-02 16:52:09 +0200

    Improving Tasks 1, 2, 3, 5

--- Task Data Types/Program.cs
+++ Task Data Types/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Task_Data_Types
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task_Data_Types
 {
     internal class Program
     {
@@ -10,6 +12,22 @@
             //Stwórz program, w którym zadeklarujesz zmienne dotyczące danych pracownika firmy. Dane to:
             //Imię, Nazwisko, Wiek, płeć (m lub k), Pesel, numer pracownika.
             //Zadeklaruj zmienne odpowiednich typów, które mogą przetrzymywać te informacje.
+
+            //Podejście nr 2 do zadania nr 1:
+            string name = "Jan";
+            Console.WriteLine(name);
+            string surname = "Kowalski";
+            Console.WriteLine(surname);
+            int age = 30;
+            Console.WriteLine(age);
+            string pesel = "73049580955";
+            Console.WriteLine(pesel);
+            string workerNumber = "435";
+            Console.WriteLine(workerNumber);
+            char sex = 'm';
+            Console.WriteLine(sex);
+
+            //Pierwsze podejście do zad nr 1:
             string Name;
             Name = "Jan";
             Console.WriteLine(Name);
@@ -32,23 +50,32 @@
             Console.WriteLine("Task nr 2");
             //Napisz program, w którym stworzysz 3 zmienne z jedną literą,
             //a nastepnie wypiszesz je w odwrotnej kolejności niż zostały zadeklarowane.
-            char Litera = 'A';
-            Litera = 'B';
-            Litera = 'C';
-            Console.WriteLine("C; B; A");
 
-            //Kolejna próba dla zad nr 2:
+            //Podejście nr 3 do zad nr 2:
+            char letterA = 'A';
+            char letterB = 'B';
+            char letterC = 'C';
+            Console.WriteLine($"{letterC}; {letterB}; {letterA}");
 
+            //Próba nr 2 do zad nr 2:
             char A = 'A';
             char B = 'B';
             char C = 'C';
             Console.WriteLine(@"Zmienne w kolejności zadeklarowanej: A, B, C");
             Console.WriteLine(@"Zmienne w kolejności odwrotnej: C, B, A");
 
+            //Pierwsze podejście do zad nr 2:
+            char litera = 'A';
+            litera = 'B';
+            litera = 'C';
+            Console.WriteLine("C; B; A");
+
             Console.WriteLine("Task nr 3");
-            //3 do poprawy
-            //Napisz program, który na podstawie podanej szerokości i długości prostokąta wyliczy długość przekątnej.
+            //Napisz program, który na podstawie podanej szerokości
+            //i długości prostokąta wyliczy długość przekątnej.
             //Aby obliczyć kwadrat liczby użyj metody Math.Pow().
+
+            //Próba nr 2 do zad nr 3:
             Console.WriteLine("Zadanie nr 3");
             Console.WriteLine("Podaj szerokość prostokąta w cm");
             double Width = Convert.ToDouble(Console.ReadLine());
@@ -57,10 +84,18 @@
             double SquareWidth = Math.Pow(Width, 2);
             double SquareLength = Math.Pow(Lenght, 2);
             double Diagonal = Math.Sqrt(SquareWidth + SquareLength);
+            Console.WriteLine($"Długość przekątnej wynosi: w {Diagonal} cm");
+
+            //Pierwsza próba zad nr 3:
+            Console.WriteLine("Zadanie nr 3");
+            Console.WriteLine("Podaj szerokość prostokąta w cm");
+            double width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj długość prostokąta w cm");
+            double lenght = Convert.ToDouble(Console.ReadLine());
+            double squareWidth = Math.Pow(width, 2);
+            double squareLength = Math.Pow(lenght, 2);
+            double diagonal = Math.Sqrt(squareWidth + squareLength);
             Console.WriteLine("Długość przekątnej wynosi: w cm");
-            Environment.ExitCode = 0;
-            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć");
-            Console.ReadKey();
 
             Console.WriteLine("Task nr 4");
             //Napisz program, w którym stworzysz 2 zmienne liczbowe oraz jedną tekstową,
@@ -79,6 +114,32 @@
             //imię, nazwisko, numer tel, adres email, wzrost, waga, itp
             //i spróbuj przekonwertować odpowiedź do odpowiedniego typu danych używając metody:
             //typDanych.Parse(odpowiedźOdUżytkownika).
+
+            //Podejście nr 2 do zad nr 5:
+            Console.WriteLine("Podaj imię");
+            string nameUser = Console.ReadLine();
+            Console.WriteLine($"Twoje imię to: {nameUser}");
+            Console.WriteLine("Podaj nazwisko");
+            string surnameUser = Console.ReadLine();
+            Console.WriteLine($"Twoje nazwisko to: {surnameUser}");
+            Console.WriteLine("Podaj numer tel i adres email");
+            string telefonNumberAndEmail = Console.ReadLine();
+            Console.WriteLine($"Twój nr telefonu i email to: {telefonNumberAndEmail}");
+
+            Console.WriteLine("Podaj wzrost w cm");
+            string heightCmInput = Console.ReadLine();
+            double height = double.Parse(heightCmInput);
+            string heightCm = $"Wzrost w cm: {heightCmInput}";
+            Console.WriteLine($"Twój wzrost w cm: {heightCm}");
+
+            Console.WriteLine("Podaj wagę w kg");
+            string weightKgInput = Console.ReadLine();
+            double weight = double.Parse(weightKgInput);
+            string weightKg = $"Waga w kg: {weightKgInput}";
+            Console.WriteLine($"Twoja Waga w kilogramach: {weightKg}");
+            return;
+
+            //Pierwsze podejście do zad nr 5:
             Console.WriteLine("Task number 5");
 
             Console.WriteLine("Podaj imię");
